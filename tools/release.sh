14c343cdf97de0dedf42cc8514132ecdab47a686
Tarjei Husøy <git@thusoy.com>
2019-05-06 11:28:01 -0700

    Improve pre-release sanity check

--- tools/release.sh
+++ tools/release.sh
@@ -27,12 +27,44 @@ clean () {
 }
 
 sanity_check () {
+    sanity_check_changelog
+    sanity_check_git_index
+}
+
+sanity_check_changelog () {
     # Ensure there are unreleased changes in the changelog
     grep --quiet 'UNRELEASED -' CHANGELOG.md \
         || (
             echo "There's no UNRELEASED section in the changelog, thus nothing to release."
             exit 1
            )
+}
+
+sanity_check_git_index () {
+    set +e
+    git diff-index --quiet --cached HEAD
+    local has_staged_changes=$?
+    git diff-files --quiet
+    local has_unstaged_changes=$?
+    set -e
+    local untracked_files="$(git clean -n hart)"
+    if [ $has_staged_changes -ne 0 ] || [ $has_unstaged_changes -ne 0 ] ; then
+        write_error "You have a dirty index, please stash or commit your changes"
+        write_error "before pushing to any of the environments"
+        exit 1
+    fi
+    if [ "$untracked_files" != "" ]; then
+        write_error "You have untracked files in the package that would be released:"
+        echo "$untracked_files"
+        exit 1
+    fi
+}
+
+write_error () {
+    local red=$(tput setaf 1)
+    local reset=$(tput sgr0)
+
+    echo >&2 "${red}$@${reset}"
 }
 
 bump_version () {
