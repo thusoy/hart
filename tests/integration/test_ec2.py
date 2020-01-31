9a0c2f32e2fb686df5b1a4b6e6f3497564872584
Tarjei Husøy <git@thusoy.com>
2020-01-31 10:15:29 -0800

    Add integration test for ec2

---
+++ tests/integration/test_ec2.py
@@ -0,0 +1,19 @@
+import pytest
+
+from hart.__main__ import main
+
+pytestmark = pytest.mark.integration
+
+
+def test_list_regions(local_config, capsys):
+    main(['-P', 'ec2', '-c', local_config, 'list-regions'])
+    captured = capsys.readouterr()
+    assert 'EU (Ireland) (eu-west-1)' in captured.out
+    assert captured.err == ''
+
+
+def test_list_zones(local_config, capsys):
+    main(['-P', 'ec2', '-c', local_config, 'list-regions', '--include-zones'])
+    captured = capsys.readouterr()
+    assert 'EU (Ireland) (eu-west-1a)' in captured.out
+    assert captured.err == ''
