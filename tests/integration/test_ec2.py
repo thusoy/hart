8b66f42f2790f4edf501e1656e5f3d589ad82374
Tarjei Husøy <git@thusoy.com>
2020-01-31 11:09:21 -0800

    Test ec2 list-sizes with region

--- tests/integration/test_ec2.py
+++ tests/integration/test_ec2.py
@@ -19,7 +19,16 @@ def test_list_zones(local_config, capsys):
     assert captured.err == ''
 
 
-def test_list_sizes(local_config, capsys):
+def test_list_sizes_without_region(local_config, capsys):
     main(['-P', 'ec2', '-c', local_config, 'list-sizes'])
     captured = capsys.readouterr()
     assert captured.out.count('2 vCPUs, 8 GB RAM, EBS only (m4.large, $72/month)') == 1
+    assert captured.err == ('No region specified, using us-east-1. Sizes listed might not exist '
+                'in any other region\n')
+
+
+def test_list_sizes(local_config, capsys):
+    main(['-P', 'ec2', '-R', 'us-west-1', '-c', local_config, 'list-sizes'])
+    captured = capsys.readouterr()
+    assert captured.out.count('2 vCPUs, 8 GB RAM, EBS only (m4.large, $84/month)') == 1
+    assert captured.err == ''
