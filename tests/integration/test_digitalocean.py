97e1104e65b38778e944131d13521e0e745c0dde
Tarjei Husøy <git@thusoy.com>
2020-01-31 10:50:26 -0800

    Add do test for list-sizes

--- tests/integration/test_digitalocean.py
+++ tests/integration/test_digitalocean.py
@@ -10,3 +10,10 @@ def test_list_regions(local_config, capsys):
     captured = capsys.readouterr()
     assert 'New York 1 (nyc1)' in captured.out
     assert captured.err == ''
+
+
+def test_list_sizes(local_config, capsys):
+    main(['-P', 'do', '-c', local_config, 'list-sizes'])
+    captured = capsys.readouterr()
+    assert captured.out.count('1 vCPUs, 1 GB RAM, 25 GB SSD (s-1vcpu-1gb, $5/month)') == 1
+    assert captured.err == ''
