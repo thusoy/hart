e20336466452a7513d61e260b143a2854f20d9f9
Tarjei Husøy <git@thusoy.com>
2021-01-31 20:35:56 +0100

    Add basic helpers for creating roles

---
+++ hart/roles.py
@@ -0,0 +1,57 @@
+import binascii
+import os
+
+from .config import load_config
+from .exceptions import UserError
+
+DEFAULT_ROLE_NAMING_SCHEME = '{unique_id}.{region}.{provider}.{role}'
+
+def get_minion_arguments_for_role(config_file, role, provider=None):
+    config = load_config(config_file)
+    core_config = config.get('hart', {})
+    role_config = config.get('roles', {}).get(role)
+    if role_config is None:
+        raise UserError('Unknown role %r, must be one of %s' % (
+            role, ', '.join(repr(r) for r in config.get('roles', {}))))
+
+    role_provider_config = role_config.get(provider, {})
+    merged_config = {}
+    merged_config.update(core_config)
+    merged_config.update(role_config)
+    merged_config.update(role_provider_config)
+
+    minion_config = {
+        # Default to keep retrying a master connection if it fails
+        'master_tries': -1,
+        'grains': {
+            'roles': [role],
+        },
+    }
+    saltmaster = core_config.get('saltmaster')
+    if saltmaster:
+        minion_config['master'] = saltmaster
+
+    return {
+        'minion_id': build_minion_id(core_config.get('role_naming_scheme', DEFAULT_ROLE_NAMING_SCHEME),
+            role=role,
+            provider=provider,
+        ),
+        'private_networking': merged_config.get('private_networking', False),
+        'provider': provider,
+        'region': merged_config.get('region'),
+        'size': merged_config.get('size'),
+        'minion_config': minion_config,
+    }
+
+
+def build_minion_id(naming_scheme, **kwargs):
+    kwargs['unique_id'] = get_unique_id()
+    try:
+        return naming_scheme.format(**kwargs)
+    except KeyError as error:
+        raise UserError('Invalid minion id template variable {%s}, must be one of %s' % (
+            error.args[0], ', '.join('{%s}' % s for s in sorted(kwargs))))
+
+
+def get_unique_id():
+    return binascii.hexlify(os.urandom(4)).decode('utf-8')
---
+++ tests/test_roles.py
@@ -0,0 +1,92 @@
+import re
+import textwrap
+from unittest import mock
+
+import pytest
+
+from hart.exceptions import UserError
+from hart.roles import get_minion_arguments_for_role, build_minion_id
+
+
+def test_create_role_provider_inheritance(named_tempfile):
+    named_tempfile.write(textwrap.dedent('''
+        [hart]
+        saltmaster = "salt.example.com"
+        role_naming_scheme = "{role}.{provider}.example.com"
+        salt_branch = "3001"
+
+        [roles.myrole]
+        private_networking = true
+
+        [roles.myrole.ec2]
+        region = "eu-central-1"
+        size = "t3.nano"
+        zone = "eu-central-1a"
+    ''').encode('utf-8'))
+    named_tempfile.close()
+
+    arguments = get_minion_arguments_for_role(named_tempfile.name, 'myrole', provider='ec2')
+    assert arguments == {
+        'minion_id': 'myrole.ec2.example.com',
+        'provider': 'ec2',
+        'region': 'eu-central-1',
+        'size': 't3.nano',
+        'private_networking': True,
+        'minion_config': {
+            'master': 'salt.example.com',
+            'master_tries': -1,
+            'grains': {
+                'roles': [
+                    'myrole',
+                ]
+            }
+        }
+    }
+
+
+def test_get_minion_arguments_for_invalid_role(named_tempfile):
+    named_tempfile.write(textwrap.dedent('''
+        [roles.myrole]
+        private_networking = true
+    ''').encode('utf-8'))
+    named_tempfile.close()
+
+    with pytest.raises(UserError, match="Unknown role 'foo', must be one of 'myrole'"):
+        get_minion_arguments_for_role(named_tempfile.name, 'foo')
+
+
+def test_build_templated_minion_id():
+    assert build_minion_id('{role}.{zone}.{provider}.example',
+        role='foo',
+        zone='bar',
+        provider='ec2',
+    ) == 'foo.bar.ec2.example'
+
+
+def test_build_unique_minion_id():
+    minion_id = build_minion_id('{unique_id}.example')
+    assert re.match(r'^[a-f0-9]{8}\.example$', minion_id) is not None
+
+
+def test_build_minion_id_invalid_parameter():
+    with pytest.raises(UserError, match=r"Invalid minion id template variable {foo}, must be one of {role}, {unique_id}"):
+        build_minion_id('{foo}', role='1')
+
+
+
+# def test_create_role(named_tempfile):
+#     named_tempfile.write(textwrap.dedent('''
+#         [hart]
+#         saltmaster = "salt.example.com"
+
+#         [roles.myrole]
+#         private_networking = true
+#         provider = "do"
+#         size = "size = "s-1vcpu-1gb""
+#         region = "sfo3"
+#     ''').encode('utf-8'))
+#     named_tempfile.close()
+
+
+#     get_minion_arguments_for_role(named_tempfile.name, 'myrole')
+#     assert isinstance(provider, DOProvider)
